681a588472a4b489c6681f15fabdbbe77535a140
Alexander Ozerski <a.ozerski2@mail.ru>
2024-04-24 00:19:29 +0300

    Задача на БФ

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const answerUser = prompt('Сколько будет 7 + 15?');
-    switch (answerUser) {
-        case '22':
-            console.log ('Вы не робот!');
-            break;
-        default:
-            const secondAnswer = prompt('Кажется вы робот..');
-            console.log(`Результат: ${secondAnswer === "Я не робот" ? 'Успех' : 'Провал'}`);
-            break;
+const balance = 1114;
+const bonusBalance = 1230;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+let message = '';
 
-    }
+let balanceReady;
+((balance > 1000) || (bonusBalance > 100)) ? balanceReady = true 
+    : balanceReady = false;
+
+if ((balanceReady === true) && (isBanned == false) && (isExist == false) && (isSelling === true)) {
+     message = 'Успешная покупка';
+} else {
+
+     message = 'Не получилось купить';
+
+}
+
+console.log (`Итог: ${message}`);
+
