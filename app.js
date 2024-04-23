69813b92dc6bb0c0ec75dc61064b979fb70988fa
Alexander Ozerski <a.ozerski2@mail.ru>
2024-04-23 23:15:32 +0300

    Задача на prompt.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const answerUser = prompt('Сколько будет 7 + 15?');
+    switch (answerUser) {
+        case '22':
+            console.log ('Вы не робот!');
+            break;
+        default:
+            const secondAnswer = prompt('Кажется вы робот..');
+            console.log(`Результат: ${secondAnswer === "Я не робот" ? 'Успех' : 'Провал'}`);
+            break;
+
+    }
