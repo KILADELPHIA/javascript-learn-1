402c4e797d544b85f41dcc583f3fd5b99de132b0
Alexander Ozerski <a.ozerski2@mail.ru>
2024-04-25 00:34:04 +0300

    cool-function

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-function logName (userName, userSurname){ ;
-     console.log(`Имя - ${userName}, Фамилия - ${userSurname} `);
+function matveyHuesosAsk (girlfriend) {
+    if (girlfriend === 'Динара') {
+        return 'Ты пидор';
+    } else {
+        return 'Норм чел'
+    }
 }
 
-logName ('Димон', 'Василич');
-
-
-function countDepositSum (depositInUSD, month, rate) {
-
-     const sum = depositInUSD * (1 + rate / 12)**month; //можно сразу вместо конста писать ретёрн =
-     return sum;
-
-}
-
-const examlpe1 = countDepositSum(1000, 54, 0.12);
-console.log (examlpe1);
-
-console.log (countDepositSum(1000,22,0.14));
+console.log(matveyHuesosAsk('Диана'));
